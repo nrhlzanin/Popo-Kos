ee8a3678a442fcc7775225ab6286a1847e7ee8dd
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2024-12-27 22:12:07 +0700

    Penambahan adminlte awal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import 'admin-lte/dist/css/adminlte.min.css';  // Untuk AdminLTE CSS
+import 'admin-lte'; // Untuk AdminLTE JS
+
+// Anda bisa menambahkan kode JS lainnya di sini jika diperlukan
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
         }),
     ],
 });
